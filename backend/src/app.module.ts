72d8c14f2eb8108bb2efd28b318b0f2f7a2ae467
Arthur <arthurelgg@gmail.com>
2023-07-16 15:40:15 -0300

    setting prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ThingsModule } from './things/things.module';
+import { CategoriesModule } from './categories/categories.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [ThingsModule],
+  imports: [ThingsModule, CategoriesModule, SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
